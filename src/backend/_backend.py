bfa31f410f41a19368ebfa9618b44a687e7a185e
Hussain Parsaiyan <hussain.parsaiyan@iceanimations.com>
2014-04-02 11:40:58 +0500

    assets referenced information is now extracted from the the scene FileInfo. This is significantly faster than the previous implementation.

--- src/backend/_backend.py
+++ src/backend/_backend.py
@@ -1,20 +1,32 @@
 from site import addsitedir as asd
-import pymel.core as pc
 asd(r"r:/Pipe_Repo/Users/Hussain/utilities/TACTIC")
 asd(r"d:/user_files/hussain.parsaiyan/Documents/trunk/work/tactic")
+
+from imaya import FileInfo as fi
 import util
 reload(util)
+
+
+import pymel.core as pc
 import os.path as op
-def get_snapshot_list(project):
-    return util.get_project_snapshots(project)
+import json
+
+def get_snapshot_list(assets):
+    snaps = []
+    map(snaps.extend, map(util.get_snapshot_from_sobject,
+                         assets))
+    return snaps
 
 def map_filename_to_snapshot(snaps):
 
     f_to_snap = {}
     
     for snap in snaps:
-        
-        f_to_snap[op.normpath(util.filename_from_snap(snap, mode = 'client_repo')).lower()] = snap
+        print snap
+        f_to_snap[op.normpath(
+            util.filename_from_snap(snap,
+                                    mode = 'client_repo')).lower()
+                  ] = snap
     return f_to_snap
 
 def reference_paths():
@@ -30,14 +42,18 @@ def check_scene(proj):
 
     refs = reference_paths()
 
-    # {ref_path: False(if ref stale)|path of upto date}
+    # {ref_node: False(if ref stale)|path of upto date}
     status = {}
     
-    snaps = get_snapshot_list(proj)
+    assets = retrieve_assets()
+    
+    snaps = get_snapshot_list(assets)
     snap_files = map_filename_to_snapshot(snaps)
 
     for ref in refs:
-        cur_snap = snap_files[op.normpath(refs[ref]).lower()]
+        cur_snap = snap_files.get(op.normpath(refs[ref]).lower())
+        if not cur_snap:
+            continue
         process = cur_snap['process'].lower()
         context = cur_snap['context'].lower()
         search_type = cur_snap['search_type'].lower()
@@ -56,6 +72,37 @@ def check_scene(proj):
                     
     return status
 
+def retrieve_assets():
+    '''
+    @return: list of TACTIC asset search keys
+    '''
+
+    # assets list to be returned
+    assets = []
+    
+    # tactic fileInfo dict
+    raw_tactic = fi.get('TACTIC')
+    if raw_tactic:
+        tactic = json.loads(raw_tactic)
+    else:
+        return assets
+    
+    server = util.get_server()
+        
+    for asset in tactic.get('assets'):
+        
+        assets.append(server.build_search_key(
+            asset.get('search_type'),
+            asset.get('search_code'),
+            project_code = asset.get('project_code')))
+    return assets
+
+
+
 def change_ref(node, newPath):
     return node.load(newFile = newPath)
+
+
+
+
 
